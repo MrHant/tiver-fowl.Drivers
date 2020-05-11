b644d6e870c10b97b1290f5248b5c56bc89484ae
Artsiomi Silivonchyk <mr.hant@gmail.com>
2020-05-11 19:18:42 +0300

    Switch project Tiver.Fowl.Drivers.Tests

--- Tiver.Fowl.Drivers/Configuration/DriversConfiguration.cs
+++ Tiver.Fowl.Drivers/Configuration/DriversConfiguration.cs
@@ -1,22 +1,27 @@
-﻿namespace Tiver.Fowl.Drivers.Configuration
+﻿using System.IO;
+using System.Reflection;
+
+namespace Tiver.Fowl.Drivers.Configuration
 {
     public class DriversConfiguration
     {
+        private string _downloadLocation;
+
         /// <summary>
         /// Location for binaries to be saved
-        /// If null - defaults to assembly location
+        /// Defaults to assembly location
         /// </summary>
-        public string DownloadLocation { get; set; }
+        public string DownloadLocation { get; set; } = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
         /// <summary>
         /// Configured driver instances
         /// </summary>
-        public DriverElement[] Instances { get; set; }
-        
+        public DriverElement[] Instances { get; set; } = {};
+
         /// <summary>
         /// Timeout to be used for HTTP requests
         /// Value in seconds
         /// </summary>
-        public int HttpTimeout { get; set; }
+        public int HttpTimeout { get; set; } = 100;
     }
 }
